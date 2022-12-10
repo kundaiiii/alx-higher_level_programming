babb6c84f1c53d669c9659a3aa052e7080a8f730
kundaiiii <kundaimm@yahoo.com>
2022-12-10 10:46:18 -0800

    tasks commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+
+    """
+
+    removes all characters 'c' and 'C' from string
+
+    """
+
+    new_str = ""
+
+    for i in my_string:
+
+        if i not in "cC":
+
+            new_str += i
+
+    return (new_str)
